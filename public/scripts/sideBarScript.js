472623dc679bd98246132e12f4b305106256aa27
leerlandais <leerlandais@gmail.com>
2024-10-01 19:16:08 +0200

    added script for sideBar and MainController

---
+++ public/scripts/sideBarScript.js
@@ -0,0 +1,24 @@
+
+// For handling of the dropdown menus in the sideBar
+// console.log("sideBarScript Active");
+document.addEventListener("DOMContentLoaded", function () {
+    const optionsWithDropdown = document.querySelectorAll(".option-with-dropdown");
+    optionsWithDropdown.forEach(function (option) {
+        option.addEventListener("click", function () {
+            const dropdown = option.querySelector(".dropdown");
+            dropdown.classList.toggle("hidden");
+        });
+    });
+
+
+    const optionsWithDropdownInner = document.querySelectorAll(".option-with-dropdown-inner");
+    optionsWithDropdownInner.forEach(function (option) {
+        option.addEventListener("click", function (event) {
+            event.stopPropagation();
+
+            const dropdownInner = option.querySelector(".dropdown-inner");
+            dropdownInner.classList.toggle("hidden");
+        });
+    });
+
+}); // end doc Ready
