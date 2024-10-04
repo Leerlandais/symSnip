b7e513e8e3bac107d0bab48d668fb9c1485513fd
leerlandais <leerlandais@gmail.com>
2024-10-04 07:06:03 +0200

    added basic admin pages

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,16 +8,38 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class AdminController extends AbstractController
 {
-# ...
+
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
             'title' => 'Administration',
-            'homepage_text' => "Bienvenue {$this->getUser()->getUsername()}",
         ]);
     }
-# ...
+
+    #[Route('/admin/add/code', name: 'admin_add_code')]
+    public function addCode(): Response
+    {
+        return $this->render('admin/addCode.html.twig', [
+            'title' => 'Administration | Add Code',
+        ]);
+    }
+
+    #[Route('/admin/add/html', name: 'admin_add_html')]
+    public function addHtml(): Response
+    {
+        return $this->render('admin/addHtml.html.twig', [
+            'title' => 'Administration | Add Html',
+        ]);
+    }
+
+    #[Route('/admin/add/exes', name: 'admin_add_exes')]
+    public function addExes(): Response
+    {
+        return $this->render('admin/addExes.html.twig', [
+            'title' => 'Administration | Add Exes',
+        ]);
+    }
 }
 
 
