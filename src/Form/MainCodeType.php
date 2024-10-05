8a6b1db6bd27155fdb1e6e0b7098f225f1847484
leerlandais <leerlandais@gmail.com>
2024-10-05 09:27:37 +0200

    added basic crud for Codes

---
+++ src/Form/MainCodeType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Html;
+use App\Entity\MainCode;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MainCodeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('code')
+            ->add('type')
+            ->add('html', EntityType::class, [
+                'class' => Html::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => MainCode::class,
+        ]);
+    }
+}
